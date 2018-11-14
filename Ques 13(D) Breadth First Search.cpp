4c2aa88f0b648a149ed566236d798536e828354f
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2018-11-14 09:24:52 +0530

    Update Ques 13(D) Breadth First Search.cpp

--- Ques 13(D) Breadth First Search.cpp
+++ Ques 13(D) Breadth First Search.cpp
@@ -1,38 +1,37 @@
 #include<iostream>
 using namespace std;
-int cost[10][10],i,j,k,n,qu[10],front,rare,v,visit[10],visited[10];
 int main()
 {
     int m;
-    cout <<"Enter no of vertices:";
+    int cost[10][10],i,j,k,n,qu[10],front,rare,v,a[10],b[10];
+    cout <<"Enter total no of vertices:";
     cin >> n;
-    cout <<"Enter no of edges:";
+    cout <<"Enter total no of edges:";
     cin >> m;
-    cout <<"\nEDGES \n";
-    for(k=1; k<=m; k++)
-    {
-        cin >>i>>j;
+    for(k=1; k<=m; k++){
+        cout<<"Enter Edges : ";
+		cin >>i>>j;
         cost[i][j]=1;
     }
-    cout <<"Enter initial vertex to traverse from:";
+    cout <<"Enter a vertex : ";
     cin >>v;
-    cout <<"Visitied vertices:";
+    cout <<"Visitied vertices : ";
     cout <<v<<" ";
-    visited[v]=1;
+    b[v]=1;
     k=1;
     while(k<n)
     {
         for(j=1; j<=n; j++)
-            if(cost[v][j]!=0 && visited[j]!=1 && visit[j]!=1)
+            if(cost[v][j]!=0 && b[j]!=1 && a[j]!=1)
             {
-                visit[j]=1;
+                a[j]=1;
                 qu[rare++]=j;
             }
         v=qu[front++];
         cout<<v <<" ";
         k++;
-        visit[v]=0;
-        visited[v]=1;
+        a[v]=0;
+        b[v]=1;
     }
 
     return 0;
